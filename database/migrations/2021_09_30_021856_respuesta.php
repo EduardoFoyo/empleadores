d13727f87ae7a14d0996203d20625a6421748b9e
Edaurdo Foyo <eduardonfoyo@gmail.com>
2021-09-29 21:33:18 -0500

    Auth y migraciones

--- database/migrations/2021_09_30_021856_respuesta.php
+++ database/migrations/2021_09_30_021856_respuesta.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Respuesta extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('respuesta', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('id_area');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('id_pregunta');
+            $table->string('respuesta');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('respuesta');
     }
 }
--- database/migrations/2021_09_30_021923_respuesta_encuestado.php
+++ database/migrations/2021_09_30_021923_respuesta_encuestado.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class RespuestaEncuestado extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('respuesta_encuestado', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('id_area');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('id_encuestado');
+            $table->bigInteger('id_respuesta');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('respuesta_encuestado');
     }
 }
